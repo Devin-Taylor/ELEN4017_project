041e9169b94d28062c08f6216d145d615f443fc7
Devin-Taylor <dev.t03@gmail.com>
2016-03-19 15:23:15 +0200

    added config file and some cofig methods

--- source/Client.go
+++ source/Client.go
@@ -1,17 +1,25 @@
 package main
 
 import (
-	"net"
+	// "net"
 	"os"
 	"fmt"
+	"io/ioutil"
+	"strings"
 )
 
 func main() {
-	if len(os.Args) != 3 {
+/*	if len(os.Args) != 3 {
 		fmt.Fprintf(os.Stderr, "Usage: %s host:port  message", os.Args[0])
 		os.Exit(1)
+	}*/
+	config_lines := readConfig()
+
+	switch os.Args[1] {
+		case "protocol": setProtocol(os.Args[2])
 	}
-	service := os.Args[1]
+
+/*	service := os.Args[1]
 	message := os.Args[2]
 
 	conn, err := net.Dial("udp", service)
@@ -24,7 +32,7 @@ func main() {
 	_, err = conn.Read(buf[0:])
 	checkError(err)
 
-	fmt.Println(string(buf[0:]))
+	fmt.Println(string(buf[0:]))*/
 
 	os.Exit(0)
 }
@@ -34,4 +42,17 @@ func checkError(err error) {
 		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
 		os.Exit(1)
 	}
+}
+
+func readConfig() []string {
+	config, err := ioutil.ReadFile("../config/connection_config.txt")
+	checkError(err)
+
+	lines := strings.Split(string(config), "\n")
+
+	return lines
+}
+
+func setProtocol(protocol string) {
+	
 }
