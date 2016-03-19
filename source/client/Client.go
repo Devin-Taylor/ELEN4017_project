3059a24ea05915d0cd324b1786d7ce503f7f6ba1
Devin-Taylor <dev.t03@gmail.com>
2016-03-19 17:49:25 +0200

    added request message code

--- source/client/Client.go
+++ source/client/Client.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	// "net"
+	"net"
 	"os"
 	"fmt"
 	// "io/ioutil"
@@ -9,6 +9,10 @@ import (
 )
 
 func main() {
+
+	service := os.Args[1]
+	connectionType := os.Args[2]
+
 	if len(os.Args) < 2 {
 		fmt.Fprintf(os.Stderr, "Usage: %s host:port  message", os.Args[0])
 		os.Exit(1)
@@ -17,31 +21,35 @@ func main() {
 	var config configSettings
 	config.initializeConfig()
 
-	switch os.Args[1] {
-		case "protocol": config.setProtocol(os.Args[2])
-		case "connection": config.setConnection(os.Args[2])
-		default: 
+	switch service {
+		case "protocol": 
+			config.protocol = connectionType
+			err := writeConfig(config)
+			checkError(err)
+			os.Exit(0)
+		case "connection": 
+			config.connection = connectionType
+			err := writeConfig(config)
+			checkError(err)
+			os.Exit(0)
+		default:
 	}
 
-	err := writeConfig(config)
+	conn, err := net.Dial(config.protocol, service)
 	checkError(err)
 
+	var request RequestMessage
+	request.setRequestLine("GET", "index.html", "HTTP/1.1")
+	request.setHeaders(os.Args[1], config.connection, "Mozilla/5.0", "en")
 
-
-/*	service := os.Args[1]
-	message := os.Args[2]
-
-	conn, err := net.Dial("udp", service)
-	checkError(err)
-
-	_, err = conn.Write([]byte(message))
+	_, err = conn.Write([]byte(request.toBytes()))
 	checkError(err)
 
 	var buf[512]byte
 	_, err = conn.Read(buf[0:])
 	checkError(err)
 
-	fmt.Println(string(buf[0:]))*/
+	fmt.Println(string(buf[0:]))
 
 	os.Exit(0)
 }
--- source/client/request_message.go
+++ source/client/request_message.go
@@ -8,3 +8,37 @@ type RequestMessage struct {
 	entityBody string
 }
 
+func (rm *RequestMessage) toString() string {
+	const sp = "\x20"
+	const lf = "\x0a"
+	const cr = "\x0d"
+	requestString := rm.method + sp
+	requestString += rm.url + sp
+	requestString += rm.version + cr + lf
+	//add header lines
+	for headerFieldName, value := range rm.headerLines {
+		requestString += headerFieldName + ":" + sp
+		requestString += value + cr + lf
+	}
+	requestString += cr + lf
+	requestString += rm.entityBody
+	return requestString
+}
+
+// Function to convert the HTTP request to bytes in the correct format
+func (rm *RequestMessage) toBytes() []byte {
+	return  []byte(rm.toString())
+}
+
+func (rm *RequestMessage) setHeaders(host string, connection string, userAgent string, language string) {
+	rm.headerLines["Host"] = host
+	rm.headerLines["Connection"] = connection
+	rm.headerLines["User-agent"] = userAgent
+	rm.headerLines["language"] = language
+}
+
+func (rm *RequestMessage) setRequestLine(method string, url string, version string) {
+	rm.method = method
+	rm.url = url
+	rm.version = version
+}
