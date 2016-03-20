98cd0a1c97e048c4cbf55f79435b045b017e06fe
Devin-Taylor <dev.t03@gmail.com>
2016-03-20 22:21:39 +0200

    added launching webpage with required response for status 200, 400, 505

--- source/client/Client.go
+++ source/client/Client.go
@@ -3,10 +3,11 @@ package main
 import (
 	"net"
 	"os"
+	"os/exec"
 	"fmt"
 	// "bufio"
-	// "io/ioutil"
-	// "strings"
+	"io/ioutil"
+	"strings"
 )
 
 func main() {
@@ -32,21 +33,27 @@ func main() {
 	checkError(err)
 	// initialize request message struct
 	request := NewRequestMessage()
+	// set request version as need to when launch 505 error later on
+	requestVersion := "HTTP/1.1"
 	// set request line information
-	request.setRequestLine(method, url, "HTTP/1.1")
+	request.setRequestLine(method, url, requestVersion)
 	// set header information
 	request.setHeaders(service, config.connection, "Mozilla/5.0", "en")
 	// write request information to the server
 	_, err = conn.Write([]byte(request.toBytes()))
 	checkError(err)
-
-
+	// call to handle server response
+	handleServer(conn, requestVersion)
 
 // for debug
-	var buf[512]byte
-	_, err = conn.Read(buf[0:])
-	checkError(err)
-	fmt.Println(string(buf[0:]))
+	// var buf[512]byte
+	// _, err = conn.Read(buf[0:])
+	// checkError(err)
+
+	// response := string(buf[0:])
+
+	
+	// fmt.Println()
 
 	os.Exit(0)
 }
@@ -78,14 +85,104 @@ func checkInput(config configSettings, service string, connectionType string) co
 func getUserInputs() (string, string) {
 	var method string
 	var url string
-
-	// reader := bufio.NewReader(os.Stdin)
     fmt.Println("Enter method: ")
     fmt.Scanf("%s", &method)
     fmt.Println("Enter URL: ")
     fmt.Scanf("%s", &url)
-    // method,_ := reader.ReadString('\n')
-    // url,_ := reader.ReadString('\n')
 
     return method, url
+}
+
+func handleServer(conn net.Conn, requestVersion string) {
+	// close the connection after this function executes
+	defer conn.Close()
+
+	// get message of at maximum 512 bytes
+	var buf [512]byte
+	// read input 
+	_, err := conn.Read(buf[0:])
+	// if there was an error exit
+	checkError(err)
+	// convert message to string and decompose it
+	response := string(buf[0:])
+
+	_, code, _, _, body := decomposeResponse(response)
+	// call the function that decides which page gets launched
+	launchPage(code, body, requestVersion)
+
+}
+
+func decomposeResponse(response string) (string, string, string, []string, string){
+		const sp = "\x20"
+		const cr = "\x0d"
+		const lf = "\x0a"
+
+		temp := strings.Split(response, cr + lf)
+		// get the request line for further processing
+		responseLine := temp[0]
+		// get the header lines 
+		// find out where the header lines end
+		var i int
+		for i = 1; i < len(temp); i++ {
+			if temp[i] == "" {
+				break
+			}
+		}
+		headerLines := temp[1:i]
+		//check if there is any content in the body
+		var bodyLines []string
+		if i  < len(temp) {
+			// get the body content
+			bodyLines = temp[i:len(temp)]
+		}
+		body := strings.Join(bodyLines, cr + lf)
+
+		// split the response line into it's components
+		responses := strings.Split(responseLine, sp)
+		status := responses[2]
+		code := responses[1]
+		version := responses[0]
+
+		return version, code, status, headerLines, body
+
+}
+
+func launchPage(code string, body string, version string) {
+
+	/*tempfile, err := ioutil.TempFile(os.TempDir(), "temp")
+	checkError(err)
+
+	defer os.Remove(tempfile.Name())*/
+
+	var content string
+
+	switch code {
+		case "505": 
+			content = fmt.Sprint("<HTML><HEAD>\n<TITLE>505 %s Not Supported</TITLE>\n</HEAD><BODY>\n<H1>505 ",version ," Not Supported</H1>\n</BODY></HTML>")
+			break
+		case "200":
+			content = body
+			break
+		case "400":
+			content = "<HTML><HEAD>\n<TITLE>400 Bad Request</TITLE>\n</HEAD><BODY>\n<H1>400 Bad Request</H1>\n</BODY></HTML>"
+			break
+		default:
+			content = "<HTML><HEAD>\n<TITLE>Request expired</TITLE>\n</HEAD><BODY>\n<H1>Request expired</H1>\n</BODY></HTML>"
+			break
+	}
+	/*_, err = tempfile.Write([]byte(content))
+	checkError(err)*/
+
+	// write contents to file
+	err := ioutil.WriteFile("../../temp/launch_file.html", []byte(content), 0644)
+	checkError(err)
+	// launch default browser with .html file
+	cmd := exec.Command("xdg-open", "../../temp/launch_file.html")
+	err = cmd.Start()
+	checkError(err)
+	// err = cmd.Wait()
+	// checkError(err)
+
+/*	err = tempfile.Close()
+	checkError(err)*/
 }
