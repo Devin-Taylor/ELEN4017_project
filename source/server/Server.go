dc5079f081b2bed33fb3fcca0ddaec8d983f3cad
Devin-Taylor <dev.t03@gmail.com>
2016-03-28 13:28:04 +0200

    debugging

--- source/server/Server.go
+++ source/server/Server.go
@@ -82,6 +82,8 @@ func handleTCPClient(conn net.Conn) {
 	for {
 		// read input 
 		_, err := conn.Read(buf[0:])
+		fmt.Println("New connection for ", conn.RemoteAddr())
+		fmt.Println("New connection for ", conn.LocalAddr())
 		// if there was an error exit
 		if err != nil {
 			return
@@ -179,7 +181,7 @@ func composeResponse(message string) *ResponseMessage{
 			// compose 404
 			response.statusCode = "404"
 			response.phrase = "Not Found"
-			response.entityBody = "<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML 2.0//EN\">\n<html>\n<head>\n<title>404 Not Found</title>\n</head>\n<body>\n<h1>Not BLAAAH Found</h1>\n<p>The requested URL " + url + " was not found on this server.</p>\n</body>\n</html>"
+			response.entityBody = "<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML 2.0//EN\">\n<html>\n<head>\n<title>404 Not Found</title>\n</head>\n<body>\n<h1>Not Found</h1>\n<p>The requested URL " + url + " was not found on this server.</p>\n</body>\n</html>"
 			// set flag
 			composeResponse = false
 		}
