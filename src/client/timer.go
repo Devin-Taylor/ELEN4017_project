1beb2d451f72e229cd5f32fc30fce03f288167bd
Devin-Taylor <dev.t03@gmail.com>
2016-04-01 00:06:20 +0200

    added detailed comments to client, timer, config_settings, decompose_response, map_handler, proxy

--- src/client/timer.go
+++ src/client/timer.go
@@ -9,17 +9,19 @@ import (
 	"io/ioutil"
 	"strings"
 )
-
+// struct containing all roundTripTimer attributed data
 type roundTripTimer struct {
 	timeMap map[string]string
 	startTime time.Time
 	duration time.Duration
 }
-
+// function responsible for initiating a new round trip timers
+// outputs - roundTripTimer: a pointer to a roundTripTimer object
 func newRoundTripTimer() *roundTripTimer {
     return &roundTripTimer{timeMap: make(map[string]string)}
 }
-
+// function responsible for loading the prexisting timer map from a file so data is not overwritten
+// inputs - mapLocation: the path to the map
 func (timer *roundTripTimer) loadTimerMap(mapLocation string) {
 
 	tempMap := make(map[string]string)
@@ -45,26 +47,26 @@ func (timer *roundTripTimer) loadTimerMap(mapLocation string) {
 
 	timer.timeMap = tempMap
 }
-
+// function starts the timer
 func (timer *roundTripTimer) startTimer() {
 	timer.startTime = time.Now()
 }
-
+// function stops the timer and calculates duration
 func (timer *roundTripTimer) stopTimer() {
 	timer.duration = time.Since(timer.startTime)
 }
-
+// function adds the new time obtained to the time map
+// inputs - callType: the characteristics corresponding to the time obtained
 func (timer *roundTripTimer) addToTimer(callType string) {
-	// timeMilli := duration.Nanoseconds()
 	timer.timeMap[callType] = timer.duration.String()
 }
-
+// function responsible for writing the new timer map to a file so the data can be analysed
+// inputs - fileLocation: the directory to the file for saving
 func (timer *roundTripTimer) writeTimerToFile(fileLocation string) {
 	var writeString string
 
 	for key, value := range timer.timeMap {
 		writeString += value + " " + key + "\n"
 	}
-
 	ioutil.WriteFile(fileLocation, []byte(writeString), 0644)
 }
